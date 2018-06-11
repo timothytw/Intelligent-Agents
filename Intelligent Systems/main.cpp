f2347a7a9c9f09ca48c475148e9e11b4ce3348ee
Timothy Wong <timothy7041@gmail.com>
2018-06-11 17:23:03 -0400

    Initial commit

--- Intelligent Systems/main.cpp
+++ Intelligent Systems/main.cpp
@@ -1,15 +1,8 @@
-//
-//  main.cpp
-//  Intelligent Systems
-//
-//  Created by Timothy Wong on 6/11/18.
-//  Copyright © 2018 Timothy Wong. All rights reserved.
-//
+// Initial commit
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
